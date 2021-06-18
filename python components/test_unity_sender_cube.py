ec5670a13e86d0623453158b032e1bdbc4b290f2
NoahMoscovici <noah.moscovici@gmail.com>
2021-06-18 15:01:38 -0700

    Update test_unity_sender_cube.py

--- python components/test_unity_sender_cube.py
+++ python components/test_unity_sender_cube.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
+"""
 
+
+ Copyright (c) 2021 Noah Moscovici, Palace Games. All right reserved.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ Version 3 as published by the Free Software Foundation; either
+ or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
 import logging
 import argparse
 import traceback
